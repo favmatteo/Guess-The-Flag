02d779522538839f723c961395236d035e64c4d3
favmatteo <matteo.favaro@outlook.com>
2023-11-04 22:52:12 +0100

    Improved image quality | changed api service

--- lib/flag_image.dart
+++ lib/flag_image.dart
@@ -9,26 +9,28 @@ class FlagImage extends StatefulWidget {
   FlagImageState instance = FlagImageState();
 
   @override
-  // ignore: no_logic_in_create_state
   State<FlagImage> createState() {
+    // ignore: no_logic_in_create_state
     return instance;
   }
 }
 
 class FlagImageState extends State<FlagImage> {
-  var country = flagData.generateNewCountry();
+  var countryCode = flagData.generateNewCountry();
 
   void update() {
     setState(() {
-      country = flagData.getCurrentCountryCode();
+      countryCode = flagData.getCurrentCountryCode();
     });
   }
 
   @override
   Widget build(context) {
-    return Image.network(
-      "https://flagsapi.com/$country/shiny/64.png",
-      scale: .1,
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Image.network(
+        "https://flagcdn.com/h240/${countryCode.toLowerCase()}.png",
+      ),
     );
   }
 }
